62d308b873a65c1e9c306aab3a766170dcd19a2d
yutamotegi <moyuta0408@gmail.com>
2020-08-27 20:03:58 +0900

    make it better

--- logic.js
+++ logic.js
@@ -1,17 +1,21 @@
 let turn = 0; //ターンの状況を持っている変数 0が初期状態,1が黒,-1が白
-let state = new Array(8); //盤面の状況を持っている配列
-let judge = "false"; //勝敗の判定 (問題6で使います)
+let state = new Array(8); //盤面の状況を持っている配列(まだ8個の配列)
+for (let i = 0; i < 8; i++) {
+  state[i] = new Array(8); //二次元配列化 (ここで8*8の配列)
+}
+let judge = "false"; //勝敗の判定 (問題4で使います)
 
 module.exports = class logic {
   //クラスは習っていないので，無視でOK
-  //通信が繋がった時にする処理
+  //通信が繋がった時orリロード時にする処理
   first() {
+    console.log("通信スタート");
     for (let i = 0; i < 8; i++) {
-      state[i] = [0, 0, 0, 0, 0, 0, 0, 0]; //stateに8つの要素を持たせて二次元配列化
+      for (let j = 0; j < 8; j++) {
+        state[i][j] = 0;
+      }
     }
-
     //問題1 : オセロの初期状態を作ってみよう
-    //ヒント　 state[a][b] = 1  でx軸にa,y軸にbの場所に黒の駒を置いている
     //------------------------------------------------------
 
     //------------------------------------------------------
@@ -20,7 +24,10 @@ module.exports = class logic {
   }
   //盤面をクリックした時にする処理
   click(x, y) {
-    if (state[x][y] == 0) {
+    console.log("今のターン" + turn);
+    console.log("クリックしたx軸:" + x);
+    console.log("クリックしたy軸:" + y);
+    if (state[y][x] == 0) {
       //クリックしたところが何も無い所で
       if (check(x, y) > 0) {
         //function check()を呼び出して戻り値によってひっくり返せるか
@@ -41,50 +48,55 @@ module.exports = class logic {
 
 function check(x, y) {
   let count = 0; //ひっくり返せる数
-  //問題3  8方向を見にいく関数を使ってみよう
-  // ヒント　revを使う count = count + rev(x, y, 0, -1);　これが上を見ている
-  //-------------------------
-
-  //-------------------------
+  count = count + change(x, y, 0, -1);
+  count = count + change(x, y, 1, -1);
+  count = count + change(x, y, 1, 0);
+  count = count + change(x, y, 1, 1);
+  count = count + change(x, y, 0, 1);
+  count = count + change(x, y, -1, 1);
+  count = count + change(x, y, -1, 0);
+  count = count + change(x, y, -1, -1);
   return count;
 }
 
-function rev(x, y, add_x, add_y) {
+function change(x, y, add_x, add_y) {
   //backupを作っておく
   let backup = new Array(8);
-  for (let i = 0; i < state.length; i++) {
+  for (let i = 0; i < 8; i++) {
     backup[i] = new Array(8);
   }
   for (let x = 0; x < 8; x++) {
     for (let y = 0; y < 8; y++) {
-      backup[x][y] = state[x][y];
+      backup[y][x] = state[y][x];
     }
   }
   let reverse_num = 0; //延長線上にある敵の駒
   let flag = 0; //ひっくり返せるかどうかを0 or 1で判断
-  let all_x = x;
-  let all_y = y;
+  let all_x = x; //クリックしたところのx軸
+  let all_y = y; //クリックしたところのy軸
 
   while (true) {
-    all_x = all_x + add_x;
-    all_y = all_y + add_y;
-    //問題4　all_xとall_yがどんな状態になったらループ処理を抜けさせたい？
-    //ヒント1 ある条件の時だけflagに1を持たせる -> コマがx,yに置けるか判定をここでしている
-    //ヒント2 6パターンあります! うまく書けば3パターンになります．
-    //----------------------------------------------
-    // if(~~~~){
-    //   break
-    // }
-    //  ある条件下の時にひっくり返せるflagを追加
-    //  if (〜〜〜〜〜) {
-    //   flag = 1;
-    //   break;
-    // }
-    //----------------------------------------------
+    all_x = all_x + add_x; //引数の分だけx軸にずらしていく
+    all_y = all_y + add_y; //引数の分だけy軸にずらしていく
+    //xが0より小さい,7より大きい,yが0より小さい,yが7より大きい時break;
+    if (all_x < 0 || all_x > 7 || all_y < 0 || all_y > 7) {
+      break;
+    }
+    //問題3 どんな時にもループを抜けさせたい？
+    //----------------------------------------------------------
 
-    state[all_x][all_y] = state[all_x][all_y] * -1;
+    //----------------------------------------------------------
+    //自分のターンならflagを1にしてループを抜ける
+    if (state[all_y][all_x] == turn) {
+      flag = 1;
+      break;
+    }
+    //上記のif3つを通らなかったらひっくり返す
+    state[all_y][all_x] = state[all_y][all_x] * -1;
+    //上記のif3つを通らなかったら何個ひっくり返せるか数える
     reverse_num++;
   }
+  //flagが0でひっくり返せない時はbackupで元の状態に戻す
   if (reverse_num > 0) {
     if (flag == 0) {
       for (let i = 0; i < 8; i++) {
@@ -94,50 +106,42 @@ function rev(x, y, add_x, add_y) {
         }
       }
     } else {
-      state[x][y] = turn;
+      //クリックした時に自分の駒を置く
+      state[y][x] = turn;
     }
   }
   return reverse_num;
 }
 
 function skip() {
-  //バックアップを取らないとcheck()によって中身が変わってしまう
-
-  //バックアップ
   let backup = new Array(8);
   for (let i = 0; i < state.length; i++) {
     backup[i] = new Array(8);
   }
   for (let x = 0; x < 8; x++) {
     for (let y = 0; y < 8; y++) {
-      backup[x][y] = state[x][y];
+      backup[y][x] = state[y][x];
     }
   }
 
   let count_can = 0;
-  //問題 5 自分のコマが置けない場合は相手のターンに変更
-  //問題 5-1 全部のマスを自分のコマがおけるかおけないかチェック
-  //ヒント check関数の再利用.全部のマスを見る為にはどうする？
-  //--------------------------------------
-
-  //--------------------------------------
-
-  //バックアップを戻す
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      count_can = count_can + check(i, j);
+    }
+  }
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
       state[i][j] = backup[i][j];
     }
   }
-
-  //問題 5-2 count_canの値によってターンを変更
-  //ヒント 5-1の値を条件分岐
-  //-------------------------------------
-
-  //-------------------------------------
+  if (count_can == 0) {
+    turn = turn * -1;
+  }
 }
 
 function win() {
-  //問題 6 勝敗を決する (ヒント無し)
+  //問題 4 勝敗を決する (ヒント無し)
   //-------------------------------------
   // return true;
   //-------------------------------------
